82beb50f1f95e5fe6ff5c42c8fa27efcd2e743ed
MrCdK <contact@mrcdk.com>
2019-06-12 12:50:52 +0200

    Generic AddPopup

--- AddPopup.gd
+++ AddPopup.gd
@@ -1,5 +1,11 @@
 extends WindowDialog
 
+export (String) var empty_name_message = ""
+export (String) var bad_name_message = ""
+export (String) var exception_message = ""
+export (String) var good_name_message = ""
+export (Database.Table) var table = Database.Table.FIGHTERS
+
 onready var NameEdit = find_node("NameEdit")
 onready var StatusLabel = find_node("StatusLabel")
 onready var OK = find_node("OK")
@@ -14,19 +20,24 @@ func check_valid():
 	var n = NameEdit.text
 	
 	valid = true
-	var msg = "The enemy needs a name"
+	var msg = empty_name_message
 	if n.empty():
 		valid = false
 		
 	if valid:
-		var exists = Database.commit(Database.Table.FIGHTERS, Database.READ, n)
+		if n.findn("_") > -1:
+			msg = exception_message
+			valid = false
+		
+	if valid:
+		var exists = Database.commit(table, Database.READ, n)
 		valid = not exists
 		if not valid:
-			msg = "That enemy already exists."
+			msg = bad_name_message
 		
 	if valid:
 		StatusLabel.modulate = Color.green
-		StatusLabel.text = "Great choice"
+		StatusLabel.text = good_name_message
 	else:
 		StatusLabel.modulate = Color.red
 		StatusLabel.text = msg
@@ -44,14 +55,17 @@ func _on_NameEdit_text_entered(new_text):
 
 func _on_OK_pressed():
 	if not valid: return
-	Database.commit(Database.Table.FIGHTERS, Database.CREATE, NameEdit.text)
+	Database.commit(table, Database.CREATE, NameEdit.text)
 	hide()
 	
-
 func _on_Cancel_pressed():
 	hide()
 
 
-func _on_AddNewEnemyPopup_about_to_show():
+func _on_AddPopup_about_to_show():
 	NameEdit.clear()
 	check_valid()
+	NameEdit.call_deferred("grab_focus")
+
+func _on_AddPopup_popup_hide():
+	pass
