d9932e8e798a80541c84b8b59609aefd5dc44538
MrCdK <contact@mrcdk.com>
2020-03-23 13:57:00 +0100

    Added the option to copy the equipment upgraded, weakened,... from the normal, upgraded or weakened ones if available.

--- equipment/TypeTabContainer.gd
+++ equipment/TypeTabContainer.gd
@@ -9,6 +9,7 @@ enum TabType {
 }
 
 export (TabType) var type = TabType.NORMAL
+export (TabType) var copy_from = TabType.NORMAL
 
 onready var TabContainer = find_node("TabContainer")
 onready var Data = find_node("Data")
@@ -18,10 +19,10 @@ onready var CreateButton = find_node("CreateButton")
 onready var CopyButton = find_node("CopyButton")
 
 var current_key:String = ""
+var current_copy = copy_from
 
 func _ready():
-	TabContainer.visible = false
-	CreateContainer.visible = true
+	_toggle_create_container(true)
 	if type == TabType.NORMAL:
 		CopyButton.visible = false
 	else:
@@ -33,23 +34,35 @@ func set_key(key:String):
 	current_key = key
 	
 	if key == null or key.empty(): 
-		TabContainer.visible = false
-		CreateContainer.visible = true
+		_toggle_create_container(true)
 		return
 	
 	var full_key = _get_full_key(type)
 	
 	var data = Database.commit(Database.Table.EQUIPMENT, Database.READ, full_key)
 	if not data: 
-		TabContainer.visible = false
-		CreateContainer.visible = true
+		_toggle_create_container(true)
 		return
-		
-	TabContainer.visible = true
-	CreateContainer.visible = false
+	
+	_toggle_create_container(false)
 	
 	Data.set_data(data)
 	Scripts.set_data(data)
+	
+func _toggle_create_container(show:bool):
+	if show:
+		TabContainer.visible = false
+		CreateContainer.visible = true
+		
+		current_copy = TabType.NORMAL
+		if Database.commit(Database.Table.EQUIPMENT, Database.READ, _get_full_key(copy_from)):
+			current_copy = copy_from
+			
+		CopyButton.text = "Copy from %s" % [TabType.keys()[current_copy].capitalize()]
+		
+	else:
+		TabContainer.visible = true
+		CreateContainer.visible = false
 	
 func _get_full_key(type):
 	var key = current_key
@@ -81,7 +94,27 @@ func _on_CreateButton_pressed():
 
 
 func _on_CopyButton_pressed():
-	pass # Replace with function body.
+	var copy_key = _get_full_key(current_copy)
+	var full_key = _get_full_key(type)
+	if Database.commit(Database.Table.EQUIPMENT, Database.READ, full_key):
+		ConfirmPopup.popup_accept("The equipment '%s' is already created." % copy_key)
+	elif Database.commit(Database.Table.EQUIPMENT, Database.CREATE, full_key):
+		var data = Database.commit(Database.Table.EQUIPMENT, Database.READ, copy_key)
+		var db_table = Database.get_table(Database.Table.EQUIPMENT)
+		for key in data:
+			if key == db_table.KEY or key.count("__") > 0:
+				continue
+			var d = data[key]
+			# if data is an array, create each value separately
+			if typeof(d) == TYPE_ARRAY:
+				for v in d:
+					Database.commit(Database.Table.EQUIPMENT, Database.CREATE, full_key, key, v)
+			else:
+				Database.commit(Database.Table.EQUIPMENT, Database.CREATE, full_key, key, d)
+		
+		set_key(current_key)
+	else:
+		ConfirmPopup.popup_accept("Something went wrong", "Ooops...")
 	
 func _on_equipment_deleted(key):
 	# Equipment was deleted, set it to empty
