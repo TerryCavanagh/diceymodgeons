1fe0a00fb1f1a0a308a6094a9dcb79635baa9b4d
MrCdK <codecdakiller@gmail.com>
2020-07-09 11:55:06 +0200

    More work on equipment tags

--- equipment/EquipmentTags.gd
+++ equipment/EquipmentTags.gd
@@ -24,39 +24,67 @@ func set_data(data):
 	
 func _setup(node, key, def):
 	if node == TagsButton:
-		var tags = data.get("Tags", def)
 		var popup:PopupMenu = TagsButton.get_popup()
 		Utils.connect_signal(popup, key, "id_pressed", self, "_on_TagsButton_popup_id_pressed")
+		load_tags()
+			
+func clear_tags():
+	for child in TagsContainer.get_children():
+		TagsContainer.remove_child(child)
+		child.queue_free()
 		
-		for idx in popup.get_item_count():
-			popup.set_item_checked(idx, Gamedata.items.default_tags.keys()[idx] in tags)
-		
-		for child in TagsContainer.get_children():
-			TagsContainer.remove_child(child)
-			child.queue_free()
-		for tag in tags:
-			add_tag(tag)
+func load_tags():
+	if not data: return
+	print("loading tags")
+	var tags = data.get("Tags", [])
+	var popup:PopupMenu = TagsButton.get_popup()
+	clear_tags()
+	for idx in popup.get_item_count():
+		popup.set_item_checked(idx, Gamedata.items.default_tags.keys()[idx] in tags)
+	for tag in tags:
+		add_tag(tag, false)
 
-func add_tag(new_tag):
+func add_tag(new_tag, to_db):
 	var label = Label.new()
-	TagsContainer.add_child(label)
 	label.text = new_tag
 	label.set_meta("tag", new_tag)
-	label.grab_focus()
-	# TODO add it to the data
+	var container:HBoxContainer = null
+	if TagsContainer.get_child_count() > 0:
+		container = TagsContainer.get_child(TagsContainer.get_child_count() - 1)
+	var lbl_size = label.get_combined_minimum_size()
+	if not container or container.rect_size.x + lbl_size.x > TagsContainer.rect_size.x:
+		container = HBoxContainer.new()
+		container.size_flags_horizontal = 0
+		TagsContainer.add_child(container)
 	
-func remove_tag(tag):
-	for child in TagsContainer.get_children():
-		if child.has_meta("tag") and child.get_meta("tag") == tag:
-			TagsContainer.remove_child(child)
-			child.queue_free()
-			# TODO remove it from the data
-			break
+	container.add_child(label)
+	
+	if to_db:
+		var tags = data.get("Tags")
+		tags.push_back(new_tag)
+		Database.commit(Database.Table.EQUIPMENT, Database.UPDATE, data_id, "Tags", tags)
+	
+func remove_tag(tag, to_db):
+	for container in TagsContainer.get_children():
+		for child in container.get_children():
+			if child.has_meta("tag") and child.get_meta("tag") == tag:
+				container.remove_child(child)
+				child.queue_free()
+				
+				if to_db:
+					var tags = data.get("Tags")
+					tags.erase(tag)
+					Database.commit(Database.Table.EQUIPMENT, Database.UPDATE, data_id, "Tags", tags)
+				
+				break
+		if container.get_child_count() == 0:
+			TagsContainer.remove_child(container)
+			container.queue_free()
 
 func _on_TagsButton_popup_id_pressed(idx, node, key):
 	node.toggle_item_checked(idx)
 	var value = Gamedata.items.default_tags.keys()[idx]
 	if node.is_item_checked(idx):
-		add_tag(value)
+		add_tag(value, true)
 	else:
-		remove_tag(value)
+		remove_tag(value, true)
