5d1d1c30aade70f19ed99ce3114da21d3e557cbb
MrCdK <codecdakiller@gmail.com>
2020-07-10 10:12:20 +0200

    Continue working on the equipment tags

--- equipment/EquipmentTags.gd
+++ equipment/EquipmentTags.gd
@@ -1,7 +1,10 @@
 extends HBoxContainer
 
+const TagContainerScene = preload("res://equipment/TagContainer.tscn")
+
 onready var TagsContainer = find_node("TagsContainer")
 onready var TagsButton = find_node("TagsButton")
+onready var TmpControl = find_node("TmpControl")
 
 var data_id:String = ""
 var data:Dictionary = {}
@@ -14,6 +17,7 @@ func _ready():
 		popup.add_check_item(Gamedata.items.default_tags.get(tag), idx)
 		popup.set_item_as_checkable(idx, true)
 		popup.set_item_tooltip(idx, tag)
+		popup.set_item_metadata(idx, tag)
 		idx += 1
 
 func set_data(data):
@@ -40,24 +44,34 @@ func load_tags():
 	var popup:PopupMenu = TagsButton.get_popup()
 	clear_tags()
 	for idx in popup.get_item_count():
-		popup.set_item_checked(idx, Gamedata.items.default_tags.keys()[idx] in tags)
+		var meta = popup.get_item_metadata(idx)
+		popup.set_item_checked(idx, meta in tags)
 	for tag in tags:
 		add_tag(tag, false)
 
 func add_tag(new_tag, to_db):
-	var label = Label.new()
-	label.text = new_tag
-	label.set_meta("tag", new_tag)
+	var tag_container = TagContainerScene.instance()
+	tag_container.connect("delete_requested", self, "remove_tag", [new_tag, true])
+	tag_container.set_meta("tag", new_tag)
+	tag_container.set_tag(new_tag, Gamedata.items.default_tags.get(new_tag, ""))
+	# To get the minimum size of the tag container we need to add it to the tree first
+	# but we can't add it directly to the container because we don't know if it will
+	# fit or not. Because of this, we add it to a tmp control to get the correct size
+	# Ideally we would make a custom container to calculate this but for this case
+	# it should be enough.
+	TmpControl.add_child(tag_container)
+	
 	var container:HBoxContainer = null
 	if TagsContainer.get_child_count() > 0:
 		container = TagsContainer.get_child(TagsContainer.get_child_count() - 1)
-	var lbl_size = label.get_combined_minimum_size()
-	if not container or container.rect_size.x + lbl_size.x > TagsContainer.rect_size.x:
+	var tag_container_size = tag_container.get_combined_minimum_size()
+	if not container or (container.rect_size.x + tag_container_size.x) > TagsContainer.rect_size.x:
 		container = HBoxContainer.new()
 		container.size_flags_horizontal = 0
 		TagsContainer.add_child(container)
 	
-	container.add_child(label)
+	TmpControl.remove_child(tag_container)
+	container.add_child(tag_container)
 	
 	if to_db:
 		var tags = data.get("Tags")
@@ -70,6 +84,12 @@ func remove_tag(tag, to_db):
 			if child.has_meta("tag") and child.get_meta("tag") == tag:
 				container.remove_child(child)
 				child.queue_free()
+				
+				var popup:PopupMenu = TagsButton.get_popup()
+				for idx in popup.get_item_count():
+					var meta = popup.get_item_metadata(idx)
+					if meta == tag:
+						popup.set_item_checked(idx, false)
 				
 				if to_db:
 					var tags = data.get("Tags")
